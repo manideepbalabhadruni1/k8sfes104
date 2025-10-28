5f049c65535c879a0527f4a23800af32d7d5b2f6
manideepbalabhadruni1 <manideepbalabhadruni@gmail.com>
2025-10-28 13:36:32 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:30083
-  /back1/auth";
+const API_URL = "http://localhost:30083/back1/auth";
+
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
@@ -10,7 +10,7 @@ export const login = async (username, password) => {
 };
 
 export const signup = async (username, email, password) => {
-  return axios.post(`${API_URL}/sign`, { username, email, password });
+  return axios.post(`${API_URL}/signup`, { username, email, password });
 };
 
 export const logout = () => {
